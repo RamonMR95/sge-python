54d444e37b0d789eb02fe4cd07ebf7fc2b6af88d
Ramon Moñino <ramonmr16@gmail.com>
2020-01-29 21:01:22 +0100

    Terminado ej 17, practica 03

--- sge-python/practica03/ej17.py
+++ sge-python/practica03/ej17.py
@@ -11,45 +11,50 @@ import random
 print("Piense un número del 1 al 100 (no me engañe ni cambie de número)")
 input("Pulse enter para comenzar")
 
-acierto = False
 
-
-def reset():
-    global n_intentos
-    global intentos
-    global limInf
-    global limSup
+def juego():
+    acierto = False
     n_intentos = 0
-    intentos = {-1}
     limInf = 1
     limSup = 100
 
+    while not acierto:
+        n_intentos += 1
 
-reset()
+        if limSup <= limInf:
+            print("No puede cambiar el número a mitad del juego")
+            n_intentos = 0
+            limInf = 1
+            limSup = 100
 
-
-while not acierto:
-    n_intentos += 1
-    num = random.randint(limInf, limSup)
-    while num in intentos:
         num = random.randint(limInf, limSup)
-        if num not in intentos:
-            intentos.add(num)
-    resp = input(f"¿es el {num}? (m-mayor, n-menor, i-igual)")
+        resp = input(f"¿es el {num}? (m-mayor, n-menor, i-igual)")
 
-    if resp == "m":
-        limInf = num + 1
-    if resp == "n":
-        limSup = num - 1
-    if resp == "i":
-        print(f"¡¡¡Qué bueno soy, lo he acertado en tan sólo {n_intentos} intentos!!!")
-        volver_jugar = input("¿Desea jugar otra vez (S/N)?").upper()
-        if volver_jugar == "N":
-            acierto = True
-            print("\nHasta la vista!!")
-        else:
-            reset()
-    if limSup - limInf <= 1:
-        print(f"No puede cambiar el número a mitad del juego")
-        acierto = True
-        reset()
+        if resp == "m":
+            if num == 100:
+                print(f"El número no puede ser mayor que 100, piense en otro número")
+                n_intentos = 0
+                limInf = 1
+                limSup = 100
+            else:
+                limInf = num + 1
+        if resp == "n":
+            if num == 1:
+                print(f"El número no puede ser menor que 0, piense en otro número")
+                n_intentos = 0
+                limInf = 1
+                limSup = 100
+            else:
+                limSup = num - 1
+        if resp == "i":
+            print(f"¡¡¡Qué bueno soy, lo he acertado en tan sólo {n_intentos} intentos!!!")
+            volver_jugar = input("¿Desea jugar otra vez (S/N)?").upper()
+            if volver_jugar == "N":
+                acierto = True
+                print("\nHasta la vista!!")
+            else:
+                n_intentos = 0
+                limInf = 1
+                limSup = 100
+
+juego()
