9609bf26a68a620b6c2e57b8312dcc0fcab1d849
Ramon Moñino <ramonmr16@gmail.com>
2020-02-11 21:05:21 +0100

    Fix ej1

--- sge-python/practica04/ej1.py
+++ sge-python/practica04/ej1.py
@@ -7,22 +7,17 @@ __version__ = "1.0.0"
 frase = input("Introduzca una frase: ").lower()
 
 # A
-print(f"A: {frase.count('a')}, E: {frase.count('e')}, I: {frase.count('i')}, O: {frase.count('o')}, U: {frase.count('u')}")
+print(f"A: {frase.count('a')}, E: {frase.count('e')}, I: {frase.count('i')}, O: {frase.count('o')}, "
+      f"U: {frase.count('u')}")
 
 # B
-lst = [0, 0, 0, 0, 0]
 
-for i in frase:
-    if i == 'a':
-        lst[0] += 1
-    elif i == 'e':
-        lst[1] += 1
-    elif i == 'i':
-        lst[2] += 1
-    elif i == 'o':
-        lst[3] += 1
-    elif i == 'u':
-        lst[4] += 1
+dic = {}
 
-print(f"A: {lst[0]}, E: {lst[1]}, I: {lst[2]}, O: {lst[3]}, U: {lst[4]}")
+for i in frase.replace(" ", ""):
+    if i not in dic:
+        dic[i] = 1
+    else:
+        dic[i] += 1
 
+print(dic)
